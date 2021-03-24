2150300a7f5b9dbba9e5f1b4d82995aa326c841c
EdmilsonESF <ef.edmilsonfonseca@gmail.com>
2021-03-24 15:36:13 -0300

    desafio concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    if (this.usersRepository.findById(user_id).admin) {
+      const users = this.usersRepository.list();
+
+      return users;
+    }
+    throw new Error("not admin");
   }
 }
 
